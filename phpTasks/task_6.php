928eafb5827abfcf5cdae4aa67b029c705f2417d
DESKTOP-868FC21\tonyr <muratgergov0820@gmail.com>
2023-01-22 23:15:52 +0300

    Выполнил 6 задание

--- phpTasks/task_6.php
+++ phpTasks/task_6.php
@@ -98,27 +98,6 @@
                     }
                         
                     ?>
-
-
-
-                        <!-- 
-                        
-                        <div class="banned rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
-                            <img src="img/demo/authors/roberto.png" alt="Jovanni Lo"
-                                 class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
-                            <div class="ml-2 mr-3">
-                                <h5 class="m-0">
-                                    Roberto R. (Rails Developer)
-                                    <small class="m-0 fw-300">
-                                        Partner &amp; Contributor
-                                    </small>
-                                </h5>
-                                <a href="https://twitter.com/@sildur" class="text-info fs-sm"
-                                   target="_blank">@sildur</a> -
-                                <a href="https://wrapbootstrap.com/user/sildur" class="text-info fs-sm" target="_blank"
-                                   title="Contact Roberto"><i class="fal fa-envelope"></i></a>
-                            </div>
-                        </div> -->
                     </div>
                 </div>
             </div>
