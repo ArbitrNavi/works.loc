8ecfb99de1d29370357440b227748cf2262245a4
tonyr <muratgergov0820@gmail.com>
2023-02-05 13:12:35 +0300

    SQL запросы

---
+++ sql.sql
@@ -0,0 +1,5 @@
+SELECT * FROM table_name WHERE table_value; --- Берём значение из таблицы
+
+SELECT 'id', 'name', 'email', 'age' FROM table_name WHERE ('[VALUE-1]', '[VALUE-2]', '[VALUE-3]', '[VALUE-4]'); --- Берём конкретные значения из таблицы
+
+INSERT INTO table_name ()
